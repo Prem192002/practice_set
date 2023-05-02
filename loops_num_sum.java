c86b8aef42807a086de47ab8243c2f1cfb15b031
Prem192002 <premsingh192002@gmail.com>
2023-05-03 01:30:22 +0530

    sum of digits

--- loops_num_sum.java
+++ loops_num_sum.java
@@ -1,23 +1,24 @@
 import java.util.Scanner;
-public class loop_num_count {
+public class loop_num_sum {
     public static void main (String [] args){
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number");
         int num = sc.nextInt();
 
-       
-        int count=0;
+        
+        int count =0;
+        int sum = 0;
         while( num>0){
 
-            
+            int digit=num%10;
             num =  num/10;
             count = count+1;
-            
+            sum = sum+digit;
 
         }
 
-        System.out.println(count);
+        System.out.println(sum);
 
 
 
