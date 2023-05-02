16b1d261d25c3058b842cca7298c871a3a02138b
Prem192002 <premsingh192002@gmail.com>
2023-05-02 22:42:12 +0530

    change condition

---
+++ loops_sum_first_n.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+class loops_sum_first_n {
+
+    public static void main(String[] args)
+    {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter the number");
+
+        int a = sc.nextInt();
+        
+        int sum = 0;
+
+        for(int num = a; num>=0 ;num=num-1) {
+            
+            
+            sum = sum+num;
+
+           
+        
+        }
+
+        System.out.println(sum);
+        
+    }
+
+    
+    
+      
+}
