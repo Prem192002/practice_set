73f8efd4395f57fe033c41083bd552c435993539
Prem192002 <premsingh192002@gmail.com>
2023-06-22 02:05:45 +0530

    prime number check

--- trial.java
+++ trial.java
@@ -1,18 +1,31 @@
-import java.util.Scanner;
-public class trial {
 
+import java.util.Scanner;
+class trial {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number:");
         int num = sc.nextInt();
 
-        int sum = 0;
-        for (int i=0;i<=num;i=i+1){
-            sum = sum+i;
-            
-        }
-        System.out.println(sum);
+        if (num>=1){
 
+            int count = 0;
+            for(int i=1;i<=num;i=i+1){
+                if(num%i==0){
+                    count=count+1;
+                }
+                
+            }
+            if(count==2){
+
+                System.out.println("Number is a prime number");
+            
+            }else{
+                System.out.println("Not a prime number");
+            }
+
+        }else{
+            System.out.println("Not a prime number");
+        }
     } 
     
 }
