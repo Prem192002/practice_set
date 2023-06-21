1827a87acc5eb9fff76444bab60c5509a904357a
Prem192002 <premsingh192002@gmail.com>
2023-06-22 03:48:13 +0530

    class function

--- trial.java
+++ trial.java
@@ -1,31 +1,13 @@
+class addition{
+    public int add(int a,int b){ 
+    int ans = a + b;
+    return ans;
+    }
+}
+public class trial{
+public static void main(String[] args){
 
-import java.util.Scanner;
-class trial {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number:");
-        int num = sc.nextInt();
-
-        if (num>=1){
-
-            int count = 0;
-            for(int i=1;i<=num;i=i+1){
-                if(num%i==0){
-                    count=count+1;
-                }
-                
-            }
-            if(count==2){
-
-                System.out.println("Number is a prime number");
-            
-            }else{
-                System.out.println("Not a prime number");
-            }
-
-        }else{
-            System.out.println("Not a prime number");
+        addition obj1 = new addition();
+        System.out.println(obj1.add(10,5));
         }
-    } 
-    
 }
