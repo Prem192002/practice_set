0e6fa1eb59da444a1e2e4ac098add863c5291935
Prem192002 <premsingh192002@gmail.com>
2023-06-26 22:46:28 +0530

    eulers algorithm

--- trial.java
+++ trial.java
@@ -1,13 +1,21 @@
-class addition{
-    public int add(int a,int b){ 
-    int ans = a - b;
-    return ans;
-    }
-}
-public class trial{
-public static void main(String[] args){
+public class trial {
 
-        addition obj1 = new addition();
-        System.out.println(obj1.add(20,5));
+    public static int phi(int n) {
+        int result = n;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                result -= result / i;
+                while (n % i == 0) {
+                    n /= i;
+                }
+            }
         }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(phi(6)); // 2
+        System.out.println(phi(10)); // 4
+        System.out.println(phi(15)); // 8
+    }
 }
