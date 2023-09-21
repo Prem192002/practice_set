0e42d4e15f6cdd9b693f9e8c24c84681679d27d5
Prem192002 <premsingh192002@gmail.com>
2023-09-21 22:07:03 +0530

    change in condition

--- conditions.java
+++ conditions.java
@@ -10,9 +10,9 @@ public class conditions {
         System.out.println("Enter the age");
         int age = sc.nextInt();
 
-        if(age<12){
+        if(age<13){
             System.out.println("childhood");
-        }else if(age>12 && age<18){
+        }else if(age>13 && age<18){
             System.out.println("teenage");
         }else{
             System.out.println("Adult");
